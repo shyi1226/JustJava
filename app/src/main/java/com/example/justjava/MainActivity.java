7b7a030fac6aaee0fa431770aa131649ce43be1b
shyi1226 <asdf951226>
2016-07-18 11:55:38 +0800

    coffee with function call

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.justjava;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,7 +11,7 @@ import java.text.NumberFormat;
 /**
  * This app displays an order form to order coffee.
  */
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     int numberOfCoffees = 0;
 
@@ -24,22 +25,22 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = numberOfCoffees * 5;
-        String priceMessage = "Total:  $" + price + "\nThank you!";
+        int price = calculatePrice(numberOfCoffees);
+        String priceMessage = createOrderSummary(price);
         displayMessage(priceMessage);
     }
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView price_text_view = (TextView) findViewById(R.id.price_text_view);
-        price_text_view.setText(message);
+        TextView order_summary_text_view = (TextView) findViewById(R.id.order_summary_text_view);
+        order_summary_text_view.setText(message);
     }
     /**
      * This method is called when the plus button is clicked. asdf
      */
     public void increment(View view) {
-        numberOfCoffees = numberOfCoffees+1;
+        numberOfCoffees = numberOfCoffees + 1;
         display(numberOfCoffees);
     }
 
@@ -59,12 +60,23 @@ public class MainActivity extends ActionBarActivity {
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
+
     /**
-     * This method displays the given price on the screen.
+     * Calculates the price of the order.
+     *
+     * @param quantity is the number of cups of coffee ordered
      */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    private int calculatePrice(int quantity) {
+        int price = quantity * 5;
+        return price;
+    }
+
+    private String createOrderSummary(int price){
+        String message = "Name: Shine Yi\n";
+        message += "Quantity: " + numberOfCoffees;
+        message += "\nTotal: $" + price;
+        message += "\nThank you!";
+        return message;
     }
 
 }
